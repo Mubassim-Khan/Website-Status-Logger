5aff7984260bbd2841e03cedd4c45fa8ae6c8853
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-02-20 17:44:43 +0000

    Fix: Change some issues

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ def git_commit_and_push():
     """Commits and pushes the log file to GitHub."""
     try:
         subprocess.run(["git", "add", LOG_FILE], check=True)
-        subprocess.run(["git", "commit", "-m", f"[Auto Commit] - log update at {datetime.now().strftime('%H:%M:%s')}"], check=True)
+        subprocess.run(["git", "commit", "-m", f"[Auto Commit] - log update at {datetime.now().strftime('%H:%M:%S')}"], check=True)
         subprocess.run(["git", "push"], check=True)
         subprocess.run(["git", "push", "--set-upstream", "origin", "main"], check=True)
-        print("Logs commited and pushed to GitHub.")
+        print(f"Logs commited and pushed to GitHub at {datetime.now().strftime('%H:%M')}.")
     except subprocess.CalledProcessError as e:
         print(f"Git error: {e}")
 
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     while True:
         check_websites()
         git_commit_and_push()
-        time.sleep(COMMIT_INTERVAL)  # Wait for 5 minutes before running again
+        time.sleep(COMMIT_INTERVAL)  # Wait for 2 minutes before running again
