8acd0e87fa084b4754c5a30d29c2a72808cdb4e4
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-02-20 17:54:36 +0000

    Refactor: Change the files structure

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-import requests
+from lib.git import git_commit_and_push
+from lib.status_checker import check_websites
 import time
-from datetime import datetime
-import subprocess
-from web_urls import WEBSITES
 
-LOG_FILE = "website_status.log"
-COMMIT_INTERVAL = 120  # Time in seconds (same as logging or adjust as needed)
+COMMIT_INTERVAL = 120  # Wait for 2 minutes before running again
 
-def check_websites():
-    with open(LOG_FILE, "a") as log:
-        for url in WEBSITES:
-            try:
-                response = requests.get(url, timeout=5)
-                status = response.status_code
-                log.write(f"{datetime.now().strftime('%d-%m-%Y %H:%M')} - URL: {url} - Status: {status}\n")
-            except Exception as e:
-                log.write(f"{datetime.now().strftime('%d-%m-%Y %H:%M')} - {url} - ERROR: {e}\n")
-
-def git_commit_and_push():
-    """Commits and pushes the log file to GitHub."""
-    try:
-        subprocess.run(["git", "add", LOG_FILE], check=True)
-        subprocess.run(["git", "commit", "-m", f"[Auto Commit] - log update at {datetime.now().strftime('%H:%M:%S')}"], check=True)
-        subprocess.run(["git", "push"], check=True)
-        subprocess.run(["git", "push", "--set-upstream", "origin", "main"], check=True)
-        print(f"Logs commited and pushed to GitHub at {datetime.now().strftime('%H:%M')}.")
-    except subprocess.CalledProcessError as e:
-        print(f"Git error: {e}")
-
-if __name__ == "__main__":
-    while True:
-        check_websites()
-        git_commit_and_push()
-        time.sleep(COMMIT_INTERVAL)  # Wait for 2 minutes before running again
+while True:
+    check_websites()
+    git_commit_and_push()
+    time.sleep(COMMIT_INTERVAL) 
