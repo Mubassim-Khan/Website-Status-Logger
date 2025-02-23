a4cd75f4f1245ee7f6860874fca7299c307e141b
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-02-23 18:31:59 +0000

    Feat: Add the Git Push for json

--- lib/git_push.py
+++ lib/git_push.py
@@ -2,14 +2,15 @@ from datetime import datetime
 import subprocess
 
 LOG_FILE = "website_status.log"
+STATUS_FILE = "statuses.json"
 
 def git_commit_and_push():
     """Commits and pushes the log file to GitHub."""
     try:
-        subprocess.run(["git", "add", LOG_FILE], check=True)
-        subprocess.run(["git", "commit", "-m", f"[Auto Commit] - Log Update at {datetime.now().strftime('%H:%M:%S')}"], check=True)
+        subprocess.run(["git", "add", LOG_FILE, STATUS_FILE], check=True)
+        subprocess.run(["git", "commit", "-m", f"[Auto Commit] - Log & Status Update at {datetime.now().strftime('%A, %d-%m-%Y, %H:%M:%S')}"], check=True)
         subprocess.run(["git", "push"], check=True)
         subprocess.run(["git", "push", "--set-upstream", "origin", "main"], check=True)
-        print(f"Logs commited and pushed to GitHub at {datetime.now().strftime('%H:%M')}.\n")
+        print(f"Files commited and pushed to GitHub at {datetime.now().strftime('%H:%M')}.\n")
     except subprocess.CalledProcessError as e:
         print(f"Git error: {e}")
