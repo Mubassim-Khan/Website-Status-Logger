139edb7b24ebd626279032959c6830e7e241185b
mubassim08 <mubassimahmedkhan@gmail.com>
2025-06-30 04:03:49 +0000

    feat: improve fetch logic & style change to dark mode

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,17 +1,20 @@
+import os, json
 from flask import Flask, render_template
-import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     try:
-        with open("statuses.json", "r") as file:
+        status_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "statuses.json")
+        with open(status_path, "r") as file:
             statuses = json.load(file)
+        last_refreshed = list(statuses["social_media"].values())[0]["last_checked"]
+        
     except FileNotFoundError:
             statuses = {"projects": {}, "social_media": {}}
 
-    return render_template('index.html', statuses = statuses) 
+    return render_template('index.html', statuses=statuses, last_refreshed=last_refreshed) 
 
 if __name__ == "__main__":
     app.run()
